48c53e276173a041b941530cac77d1358ce732ff
Mrock <jkmilo1030@gmail.com>
2025-08-08 19:04:12 -0500

    Se elimino un test y se añadio como una función a los test negativos

--- test/test_login_page_negative.py
+++ test/test_login_page_negative.py
@@ -4,18 +4,26 @@ from selenium.webdriver.common.by import By
 import time 
 import pytest
 
+@pytest.mark.fixture()
+def driver():
 
-
+    print ("Creating chorme driver")
+    service = Service("/usr/bin/chromedriver")  # Ajusta si es necesario
+    my_driver = webdriver.Chrome(service=service)
+    #add yield if u want return something and keep write code in the some function
+    yield my_driver
+    print ("Closing chorme driver")
+    my_driver.quit()
+    
 class TestNegativeScenarios:
     
     @pytest.mark.login
     @pytest.mark.negative
     
     def test_negative_username(self):
-        
         service = Service("/usr/bin/chromedriver")  # Ajusta si es necesario
         driver = webdriver.Chrome(service=service)
-        time.sleep(1)
+        
         #open Page
         driver.get("https://practicetestautomation.com/practice-test-login/")
         
@@ -41,6 +49,38 @@ class TestNegativeScenarios:
         #we put the assert 
         assert actual_text == "Your username is invalid!", "Error message is not expected"
         time.sleep(1)
+        
+        
+    @pytest.mark.login
+    @pytest.mark.negative     
+    def test_negative_password(self):
+        #open the driver 
+        service = Service("/usr/bin/chromedriver")  # Ajusta si es necesario
+        driver = webdriver.Chrome(service=service)
+        time.sleep(5)
+        #go to the page
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+
+        #Type username student into Username field
+        username_locator = driver.find_element(By.XPATH, "/html/body/div/div/section/section/div[1]/div[1]/input")
+        username_locator.send_keys("student")
+
+        #Type password Password123 into Password field
+        password_locator= driver.find_element(By.XPATH,'/html/body/div/div/section/section/div[1]/div[2]/input')
+        password_locator.send_keys("Prueba")
+
+        #Push Submit button
+        submit_button_locator = driver.find_element(By.ID,'submit')
+        submit_button_locator.click()
+        time.sleep(1)
+            
+         #Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        text_locator= driver.find_element(By.ID,'error')
+        #use ".text" for subtract to text of text locator
+        actual_text= text_locator.text
+        #we put the assert 
+        assert actual_text == "Your password is invalid!", "Error message is not expected"
             
 
           
