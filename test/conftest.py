77340977a5bba0d4e2ffcc07dd9a001b0ecc9669
Mrock1030 <jkmilo1030@gmail.com>
2025-08-28 01:41:17 -0500

    Añadimos mejoras para ejecutar test with both browser

--- test/conftest.py
+++ test/conftest.py
@@ -1,17 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 #from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 import pytest
 
 
 @pytest.fixture()
-def driver():
-
-    print ("Creating chorme driver")
-    service = Service("/usr/bin/chromedriver") 
-    my_driver = webdriver.Chrome(service=service)
+def driver(request):
+    #found the browser
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver ")
+    
+    if browser == "chrome":
+        service = Service("/usr/bin/chromedriver") 
+        my_driver = webdriver.Chrome(service=service)
+    elif browser == "firefox":
+        my_driver= webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    else:
+        raise TypeError (f"Expected chrome  or firefox, but got {browser} ")
+    
     #add yield if u want return something and keep write code in the same function
     yield my_driver
-    print ("Closing chorme driver")
+    print (f"Closing {browser} driver")
+    
     my_driver.quit()
     
